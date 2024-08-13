c3f246f9d5e0552d721a41ac98d84ec662e28258
Aravind <100823292+Aravind-psiog@users.noreply.github.com>
2024-08-13 22:26:22 +0530

    Added dictionary

---
+++ dictBasics.py
@@ -0,0 +1,30 @@
+def dictoperation():
+    myDict = {"name": "Aravind", "age": 28,
+              "location": "Kodaikanal", "temp": 98.45, "unit": "celsius"}
+    newDict = myDict.copy()
+    print(myDict.get("location"))
+    myDict.pop("name")
+    print(myDict)
+    print(myDict.values())
+    print(myDict.keys())
+    print(newDict.items())
+    newDict.update({"weight": 86})
+    newDict.update({"temp": 98})
+    print(newDict)
+    newDict.popitem()
+    print(newDict)
+    newDict.setdefault("remote", "Chennai")
+    print(newDict)
+
+    for k, v in newDict.items():
+        print(k, v)
+
+
+def fromkey():
+    i = ("name", "age", "location")
+    j = None
+    newDict = dict.fromkeys(i, j)
+    print(newDict)
+
+
+fromkey()
--- listBasics.py
+++ listBasics.py
@@ -1,9 +1,20 @@
 def list():
     thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+    newList = thislist.copy()
+    print(thislist[1:4])
     thislist[5] = "Raspberry"
     thislist[1:4] = ["blackcurrant", "watermelon"]
     thislist.insert(3, "asd")
     print(thislist)
+    if thislist.count("watermelon") == 1:
+        print(thislist.index("watermelon"))
+    newList.pop(1)
+    newList.remove("cherry")
+    print(newList)
+    newList.reverse()
+    print(newList)
+    newList.sort()
+    print(newList)
 
 
 list()
